9cef1bb4ec6d6c16a34aa5523e3035ffa9b69463
Raphael Zanarelli <raphael.zanarelli@s2it.com.br>
2018-05-24 15:07:53 -0300

    aula 2 javascript

--- index.js
+++ index.js
@@ -1,31 +1,55 @@
-document.addEventListener("DOMContentLoaded", function(){
-    document.querySelector("#careStoreSubmit").addEventListener("submit",function(e){
+var LandingPage = (function () {
+
+
+    var form = document.querySelector("#careStoreSubmit");
+
+    function init() {
+        initEvents();
+    }
+
+    function initEvents() {
+
+        form.addEventListener("submit", onSubmit);
+    }
+
+    function onSubmit(e) {
+        console.log(this);
         e.preventDefault();
-        var campoNome = document.querySelector("#name");
-        var campoEmail = document.querySelector("#email");
-        var campoSugestao = document.querySelector("#sugestao");
-        var campos = [campoNome, campoEmail, campoSugestao];
+        // var campoNome = document.querySelector("#name");
+        // var campoEmail = document.querySelector("#email");
+        // var campoSugestao = document.querySelector("#sugestao");
+        
+        var arr = document.querySelectorAll('.input');
 
-        var podeEnviar = isNotEmpty(campos);
 
-        if(podeEnviar){
+        var podeEnviar = isNotEmpty(arr);
+
+        if (podeEnviar) {
             this.submit();
         }
-    });
-});
+    }
 
-function isNotEmpty(campos) {
-    var podeEnviar = true;
-    campos.forEach(function(e){
-        if(e.value.length === 0){
-            console.error("Campo " + e.id + "Vazio");
-            e.classList.add('formu');
-            podeEnviar = false;
-            return;
-        }
-        else {
-            e.classList.remove('formu');
-        }
-    });
-    return podeEnviar;
-}
+    function isNotEmpty(fields) {
+        var podeEnviar = true;
+        fields.forEach(function (e) {
+            if (e.value.length === 0) {
+                console.error("Campo " + e.id + "Vazio");
+                e.classList.add('formu');
+                podeEnviar = false;
+                return;
+            }
+            else {
+                e.classList.remove('formu');
+            }
+        });
+        return podeEnviar;
+    }
+
+    return {
+        init
+    }
+
+})();
+
+
+document.addEventListener("DOMContentLoaded", LandingPage.init);
